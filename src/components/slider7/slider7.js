ab528b87d4b3bdc32349b18b8d59b26bf2ba93dc
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2024-09-05 10:33:58 +0200

    updating main

--- src/components/slider7/slider7.js
+++ src/components/slider7/slider7.js
@@ -165,7 +165,7 @@ export default function Fnc(props) {
     if (distancePoints > 0 || distancePoints < 0) {
       //changed HERE to include the OR - otherwise, it only went right
       newEnd = getOverlapBorder(newEnd, true);
-      newStart = changedItem.Start + (newEnd - changedItem.End);
+      newStart = getOverlapBorder(newStart, true);
     } else {
       newStart = getOverlapBorder(newStart, false); // Here?  changes overlap rules
       newEnd = changedItem.End + (newStart - changedItem.Start);
@@ -180,12 +180,12 @@ export default function Fnc(props) {
     setDatasource(newState);
   }
 
-  function getOverlapBorder(newTime, directionUp) {
+  function getOverlapBorder(newTime, directionRight) {
     for (let index = 0; index < datasource.length; index++) {
       const element = datasource[index];
       if (element.ID != selectedItem.ID) {
         if (newTime > element.Start && newTime < element.End)
-          return directionUp ? element.Start : element.End;
+          return directionRight ? element.Start : element.End;
       }
     }
     return newTime;
